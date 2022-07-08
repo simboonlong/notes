81d752b5a23584767dc3ed875fdefd8a58c4733b
Sim Boon Long <simboonlong@gmail.com>
2022-07-08 21:27:11 +0800

    fix: remove round

--- integration/home.spec.ts
+++ integration/home.spec.ts
@@ -17,10 +17,8 @@ test("should scroll to content, on quick links click", async (t) => {
   await t.click(sidebar.find("a").withExactText("Code"));
   await t.wait(2000);
 
-  const st = Math.round(await getScrollTop());
-  const elTop = Math.round(
-    (await main.find("#code").offsetTop) + (await Selector("#main").offsetTop),
-  );
-
+  const st = await getScrollTop();
+  const elTop =
+    (await main.find("#code").offsetTop) + (await Selector("#main").offsetTop);
   await t.expect(st).eql(elTop);
 });
